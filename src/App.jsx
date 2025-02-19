5f5dc70e65e9781696c10b956ab2a08b7aa5d560
abhishekdav003 <abhishekdav003@gmail.com>
2025-02-19 15:33:30 +0530

    content updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
-
 const App = () => {
   return (
-    <div className="flex flex-col items-center">
-      {/* <Header /> */}
+    <div> 
+      <Header />
       <Footer />
-
     </div>
   );
 };
 
 export default App;
-
