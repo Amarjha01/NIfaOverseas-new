692c3c66cabcc3cc0b040da51213ff4434a4db4f
Amar <amarjha880@gmail.com>
2025-02-19 10:17:19 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Footer from "./components/Footer";
+import HeroSection from "./components/HeroSection";
 
 const App = () => {
   return (
     <div className="flex flex-col items-center">
+      <HeroSection />
       <Footer />
     </div>
   );
