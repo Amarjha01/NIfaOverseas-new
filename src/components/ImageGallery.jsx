1fdc1961623ee9433e41be6ee1dc8d8c984878ca
abhishekdav003 <abhishekdav003@gmail.com>
2025-03-06 15:28:00 +0530

    final code

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -19,18 +19,18 @@ const imagesDesktop = [
 ];
 
 const imagesMobile = [
-  'https://res.cloudinary.com/dvlh2e6d0/image/upload/v1740132033/pyxxjrxfd72rgwytn7as.jpg',
-  'https://res.cloudinary.com/dvlh2e6d0/image/upload/v1740132025/gfcvzxoqghtaltovscp1.jpg',
-  'https://res.cloudinary.com/dbnticsz8/image/upload/v1740373636/febTech/Nifa/qqal4p1fwbtaagiefv87.jpg',
-  'https://res.cloudinary.com/dvlh2e6d0/image/upload/v1740132023/kae92swwd1dedawgajk2.jpg',
-  'https://res.cloudinary.com/dvlh2e6d0/image/upload/v1740132023/ldzcizpxykzpi2klpxwj.jpg',
-  'https://res.cloudinary.com/dbnticsz8/image/upload/v1740374293/febTech/Nifa/wbqtykgeuqwe7eiz66tl.jpg',
-  'https://res.cloudinary.com/dvlh2e6d0/image/upload/v1740132022/i8tiuzrxecnr3iv7kh1z.jpg',
-  'https://res.cloudinary.com/dbnticsz8/image/upload/v1740373245/febTech/Nifa/nbac1t3kv6clva4lrwxx.jpg',
-  'https://res.cloudinary.com/dvlh2e6d0/image/upload/v1740132021/b630tkpmwxbwmuqcfdlm.jpg',
-  'https://res.cloudinary.com/dbnticsz8/image/upload/v1740374001/febTech/Nifa/vxjvrzwndyzgwyacexdl.jpg',
-  'https://res.cloudinary.com/dbnticsz8/image/upload/v1740372468/febTech/Nifa/lfhkoiqvmhn4hbouhjd0.jpg',
-  'https://res.cloudinary.com/dbnticsz8/image/upload/v1740372809/febTech/Nifa/zgpyqf7cew0lcfarvidk.jpg',
+  'https://res.cloudinary.com/dkdyrgg3q/image/upload/v1741250752/Nifa%20Overseas/t3flkcyplyppurtcryyr.jpg',
+  'https://res.cloudinary.com/dkdyrgg3q/image/upload/v1741067777/Nifa%20Overseas/s9ufsgt69jzulu9qd5py.jpg',
+  'https://res.cloudinary.com/dkdyrgg3q/image/upload/v1741067786/Nifa%20Overseas/hpylhg5hjtjxxqw3uzad.jpg',
+  'https://res.cloudinary.com/dkdyrgg3q/image/upload/v1741250781/Nifa%20Overseas/xalz3klj7eruarnhjwn4.jpg',
+  'https://res.cloudinary.com/dkdyrgg3q/image/upload/v1741067805/Nifa%20Overseas/uwy8jxye2thjfbdaesgl.jpg',
+  'https://res.cloudinary.com/dkdyrgg3q/image/upload/v1741067811/Nifa%20Overseas/qkiscc8y9ioog86w9qev.jpg',
+'https://res.cloudinary.com/dkdyrgg3q/image/upload/v1741067818/Nifa%20Overseas/oozdepuikmd5rfkldrst.jpg',
+  'https://res.cloudinary.com/dkdyrgg3q/image/upload/v1741067829/Nifa%20Overseas/efqv8xvg8g5r3bhxdid1.jpg',
+  'https://res.cloudinary.com/dkdyrgg3q/image/upload/v1741067835/Nifa%20Overseas/hzbxfezloyk0e1gsjuv3.jpg',
+  'https://res.cloudinary.com/dkdyrgg3q/image/upload/v1741067840/Nifa%20Overseas/hyundanpljdsgqnhoxt9.jpg',
+  'https://res.cloudinary.com/dkdyrgg3q/image/upload/v1741067844/Nifa%20Overseas/bwbwft1z6j8w9naxydnx.jpg',
+  'https://res.cloudinary.com/dkdyrgg3q/image/upload/v1741067851/Nifa%20Overseas/e4jql9wg7g5ecn1fwyhy.jpg',
 ];
 
 const ImageGallery = () => {
