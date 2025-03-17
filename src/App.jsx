6583243003fe74067b6f0f5d393c93171e4032ca
AtulKuma-Seo <kumarfebtech@gmail.com>
2025-03-17 15:28:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -29,18 +29,23 @@ const App = () => {
       }} />}
       <Helmet>
         {/* Global SEO metadata */}
-        <title>Wholesale Handmade Metal Décor, Indian Handicrafts</title>
+      	<title>Wholesale Handmade Metal Décor, Indian Handicrafts</title>
         <meta name="description" content="Discover stunning handmade metal décor at wholesale prices. Shop authentic Indian handicrafts and metal decorative items for your business today!" />
+		    <link rel="canonical" href="https://nifaoverseas.com/" />
         <meta name="keywords" content="Metal decorative items, handicraft products, handcrafted products, Best handicraft metal home decor wholesale, Wholesale handmade products, Handicrafts wholesale online in India" />
         <meta name="author" content="Tabrez Mirza" />
         <meta name="robots" content="index, follow" />
 
         {/* Open Graph */}
-        <meta property="og:title" content="My Website - React App" />
-        <meta property="og:description" content="My awesome React website built for learning SEO" />
-        <meta property="og:image" content="https://example.com/og-image.jpg" />
-        <meta property="og:url" content="https://example.com" />
+        <meta property="og:title" content="Wholesale Handmade Metal Décor, Indian Handicrafts" />
+        <meta property="og:description" content="Discover stunning handmade metal décor at wholesale prices. Shop authentic Indian handicrafts and metal decorative items for your business today" />
+        <meta property="og:url" content="https://nifaoverseas.com/" />
         <meta property="og:type" content="website" />
+		    <meta property="og:site_name" content="nifaoverseas" />
+        <meta property="og:image" content="https://res.cloudinary.com/dbnticsz8/image/upload/v1742204675/febTech/Nifa/kb9p9mnwrz5ruhposfg3.jpg" />
+		    <meta property="og:image:width" content="800" />
+		    <meta property="og:image:height" content="600" />
+		    <meta property="og:image:type" content="image/jpeg" />
       </Helmet>
       <FloatBtn />
       <Header />
