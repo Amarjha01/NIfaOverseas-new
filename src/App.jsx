9e4a81bc8fd7d6cc79f4953b3b32bf19bd854441
AtulKuma-Seo <kumarfebtech@gmail.com>
2025-03-17 16:01:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -29,23 +29,23 @@ const App = () => {
       }} />}
       <Helmet>
         {/* Global SEO metadata */}
-      	<title>Wholesale Handmade Metal Décor, Indian Handicrafts</title>
-        <meta name="description" content="Discover stunning handmade metal décor at wholesale prices. Shop authentic Indian handicrafts and metal decorative items for your business today!" />
-		    <link rel="canonical" href="https://nifaoverseas.com/" />
-        <meta name="keywords" content="Metal decorative items, handicraft products, handcrafted products, Best handicraft metal home decor wholesale, Wholesale handmade products, Handicrafts wholesale online in India" />
+	<title>Wholesale Handmade Bells & Décor – Artisan Craftsmanship</title>
+        <meta name="description" content="Shop handcrafted bells & aesthetic home décor at wholesale prices. Perfect for Christmas, e-commerce, and retailers seeking authentic artisan craftsmanship." />
+	<link rel="canonical" href="https://nifaoverseas.com/" />
+        <meta name="keywords" content="home decoration, aesthetic, bell, bells, e-commerce, artisan, craftsmanship, Christmas wholesale price, Handicrafts wholesale online in India" />
         <meta name="author" content="Tabrez Mirza" />
         <meta name="robots" content="index, follow" />
 
         {/* Open Graph */}
-        <meta property="og:title" content="Wholesale Handmade Metal Décor, Indian Handicrafts" />
-        <meta property="og:description" content="Discover stunning handmade metal décor at wholesale prices. Shop authentic Indian handicrafts and metal decorative items for your business today" />
+        <meta property="og:title" content="Wholesale Handmade Bells & Décor – Artisan Craftsmanship" />
+        <meta property="og:description" content="Shop handcrafted bells & aesthetic home décor at wholesale prices. Perfect for Christmas, e-commerce, and retailers seeking authentic artisan craftsmanship." />
         <meta property="og:url" content="https://nifaoverseas.com/" />
         <meta property="og:type" content="website" />
-		    <meta property="og:site_name" content="nifaoverseas" />
+	<meta property="og:site_name" content="nifaoverseas" />
         <meta property="og:image" content="https://res.cloudinary.com/dbnticsz8/image/upload/v1742204675/febTech/Nifa/kb9p9mnwrz5ruhposfg3.jpg" />
-		    <meta property="og:image:width" content="800" />
-		    <meta property="og:image:height" content="600" />
-		    <meta property="og:image:type" content="image/jpeg" />
+	<meta property="og:image:width" content="800" />
+	<meta property="og:image:height" content="600" />
+	<meta property="og:image:type" content="image/jpeg" />
       </Helmet>
       <FloatBtn />
       <Header />
